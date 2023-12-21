49621f6e1faa55cfac84a7f66b9742863c0ee89c
Aziz Limonu <azizlimonu@gmail.com>
2023-12-22 07:39:20 +0800

    add post overlay

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -3,6 +3,7 @@ import { ref, onMounted, toRefs } from "vue";
 import { Head, Link, router } from "@inertiajs/vue3";
 
 import MainLayout from "@/Layouts/MainLayout.vue";
+import ShowPostOverlay from "@/Components/ShowPostOverlay.vue";
 
 import "vue3-carousel/dist/carousel.css";
 import { Carousel, Slide, Navigation } from "vue3-carousel";
@@ -12,7 +13,17 @@ import DotsHorizontal from "vue-material-design-icons/DotsHorizontal.vue";
 let wWidth = ref(window.innerWidth);
 let currentSlide = ref(0);
 let currentPost = ref(null);
-let openOverlay = ref(false);
+let openOverlay = ref(true);
+
+const addComment = () => {
+  console.log("ADD");
+};
+const updateLike = () => {
+  console.log("Update");
+};
+const deleteFunc = () => {
+  console.log("Delete");
+};
 
 onMounted(() => {
   window.addEventListener("resize", () => {
@@ -62,9 +73,74 @@ onMounted(() => {
           <Navigation />
         </template>
       </Carousel>
+
+      <div
+        id="Posts"
+        class="px-4 max-w-[600px] mx-auto mt-10"
+        v-for="post in 1"
+        :key="post"
+      >
+        <div class="flex items-center justify-between py-2">
+          <div class="flex items-center">
+            <Link href="/" class="flex items-center">
+              <img class="rounded-full w-[38px] h-[38px]" src="/19.jpg" />
+              <div class="ml-4 font-extrabold text-[15px]">
+                This wo has the post
+              </div>
+            </Link>
+            <div class="flex items-center text-[15px] text-gray-500">
+              <span class="-mt-5 ml-2 mr-[5px] text-[35px]">.</span>
+              <div>21 Dec 2023</div>
+            </div>
+          </div>
+
+          <DotsHorizontal class="cursor-pointer" :size="27" />
+        </div>
+
+        <div class="bg-black rounded-lg w-full min-h-[400px] flex items-center">
+          <img class="mx-auto w-full" src="/boho.png.jpg" />
+        </div>
+
+        <LikesSection :post="post" @like="updateLike($event)" />
+
+        <div class="text-black font-extrabold py-1">20 likes</div>
+        <div>
+          <span class="text-black font-extrabold"> This wo has the post </span>
+          Caption Here
+        </div>
+        <button
+          @click="
+            currentPost = post;
+            openOverlay = true;
+          "
+          class="text-gray-500 font-extrabold py-1"
+        >
+          View all 5 comments
+        </button>
+      </div>
+
+      <div class="pb-20"></div>
     </div>
   </MainLayout>
-  <p>THIS IS DO</p>
+
+  <ShowPostOverlay
+    v-if="openOverlay"
+    :post="currentPost"
+    @addComment="addComment($event)"
+    @updateLike="updateLike($event)"
+    @deleteSelected="deleteFunc($event)"
+    @closeOverlay="openOverlay = false"
+  />
 </template>
 
-<style></style>
+<style>
+.carousel__prev,
+.carousel__next,
+.carousel__prev:hover,
+.carousel__next:hover {
+  color: rgb(49, 49, 49);
+  background-color: rgb(255, 255, 255);
+  border-radius: 100%;
+  margin: 0 20px;
+}
+</style>
