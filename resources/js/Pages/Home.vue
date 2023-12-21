cb485cbc8a822c1035590c7b16c0a7f4f2d4951a
Aziz Limonu <azizlimonu@gmail.com>
2023-12-21 16:56:48 +0800

    fix main layout

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -15,58 +15,56 @@ let currentPost = ref(null);
 let openOverlay = ref(false);
 
 onMounted(() => {
-    window.addEventListener("resize", () => {
-        wWidth.value = window.innerWidth;
-    });
+  window.addEventListener("resize", () => {
+    wWidth.value = window.innerWidth;
+  });
 });
-
-const allUsers = [{}];
 </script>
 <template>
-    <Head title="Instagram" />
+  <Head title="Instagram" />
 
-    <MainLayout>
-        <div class="mx-auto lg:pl-0 md:pl-[80px] pl-0">
-            <Carousel
-                v-model="currentSlide"
-                class="max-w-[700px] mx-auto"
-                :items-to-show="wWidth >= 768 ? 8 : 6"
-                :items-to-scroll="4"
-                :wrap-around="true"
-                :transition="500"
-                snapAlign="start"
+  <MainLayout>
+    <div class="mx-auto lg:pl-0 md:pl-[80px] pl-0">
+      <Carousel
+        v-model="currentSlide"
+        class="max-w-[700px] mx-auto"
+        :items-to-show="wWidth >= 768 ? 8 : 6"
+        :items-to-scroll="4"
+        :wrap-around="true"
+        :transition="500"
+        snapAlign="start"
+      >
+        <Slide v-for="slide in 10" :key="slide">
+          <Link
+            href="/"
+            class="relative mx-auto text-center mt-4 px-2 cursor-pointer"
+          >
+            <div
+              class="absolute z-[-1] -top-[5px] left-[4px] rounded-full rotate-45 w-[64px] h-[64px] contrast-[1.3] bg-gradient-to-t from-yellow-300 to-purple-500 via-red-500"
             >
-                <Slide v-for="slide in 10" :key="slide">
-                    <Link
-                        :href="route('users.show', { id: slide.id })"
-                        class="relative mx-auto text-center mt-4 px-2 cursor-pointer"
-                    >
-                        <div
-                            class="absolute z-[-1] -top-[5px] left-[4px] rounded-full rotate-45 w-[64px] h-[64px] contrast-[1.3] bg-gradient-to-t from-yellow-300 to-purple-500 via-red-500"
-                        >
-                            <div
-                                class="rounded-full ml-[3px] mt-[3px] w-[58px] h-[58px] bg-white"
-                            />
-                        </div>
-                        <img
-                            class="rounded-full w-[56px] h-[56px] -mt-[1px]"
-                            :src="slide.file"
-                        />
-                        <div
-                            class="text-xs mt-2 w-[60px] truncate text-ellipsis overflow-hidden"
-                        >
-                            {{ slide.name }}
-                        </div>
-                    </Link>
-                </Slide>
+              <div
+                class="rounded-full ml-[3px] mt-[3px] w-[58px] h-[58px] bg-white"
+              />
+            </div>
+            <img
+              class="rounded-full w-[56px] h-[56px] -mt-[1px]"
+              src="/user-placeholder.png"
+            />
+            <div
+              class="text-xs mt-2 w-[60px] truncate text-ellipsis overflow-hidden"
+            >
+              <!-- {{ slide.name }} -->
+            </div>
+          </Link>
+        </Slide>
 
-                <template #addons>
-                    <Navigation />
-                </template>
-            </Carousel>
-        </div>
-    </MainLayout>
-    <p>THIS IS DO</p>
+        <template #addons>
+          <Navigation />
+        </template>
+      </Carousel>
+    </div>
+  </MainLayout>
+  <p>THIS IS DO</p>
 </template>
 
 <style></style>
