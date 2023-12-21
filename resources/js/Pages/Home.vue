e1165f0489e68bf0f2be754bb599c3a6f47716e0
Aziz Limonu <azizlimonu@gmail.com>
2023-12-21 13:58:03 +0800

    setup vue

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MainLayout from "@/Layouts/MainLayout.vue";
+</script>
+<template>
+    <Head title="HOME" />
+    <MainLayout>
+        <div class="bg-green-500">Hello World</div>
+    </MainLayout>
+</template>
+
+<style></style>
